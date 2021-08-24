9a18d0525c7753e6beaeae93d66e5402bef61ce2
Lloyd <lloyddsouza@dsmartsys.in>
2021-08-24 14:01:08 +0530

    Move payloads to separate folder

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const homePayload = {"home":{"logoWithName":"https://fakeimg.pl/274x49","annoucements":[{"title":"xyz announcement","desc":"xyz description","from":"2021-08-22T20:25:09+0000","to":"2021-08-22T20:25:09+0000"},{"title":"abc announcement","desc":"abc description","from":"2021-08-22T20:25:09+0000","to":"2021-08-22T20:25:09+0000"},{"title":"pqr announcement","desc":"pqr description","from":"2021-08-22T20:25:09+0000","to":"2021-08-22T20:25:09+0000"}],"tiles":[{"order":1,"title":"Member Login","desc":"Manage your account","icon":"face","url":"www.google.com","backgroundColor":"#efcced"},{"order":2,"title":"Schedule","desc":"calendar updates","icon":"schedule","url":"www.google.com","backgroundColor":"#efcced"},{"order":3,"title":"Facebook","desc":"Follow us on Facebook","icon":"schedule","url":"www.google.com","backgroundColor":"#efcced"},{"order":4,"title":"Card","desc":"Your membership","icon":"qr_code_scanner","url":"www.google.com","backgroundColor":"#efcced"}],"config":{"primaryColor":"#796AC6","secondaryColor":"#FEB144","primaryTextColor":"#ffffff","secondaryTextColor":"#000000"}}}
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/home', (req, res) => {
+  const homePayload = require('./payloads/home');
   res.send(homePayload);
 })
 
---
+++ payloads/home.js
@@ -0,0 +1,66 @@
+module.exports.home = {
+  "logoWithName": "https://fakeimg.pl/274x49",
+  "annoucements": [
+    {
+      "thumbnail": "https://fakeimg.pl/60x60",
+      "title": "xyz announcement",
+      "desc": "xyz description",
+      "from": "2021-08-22T20:25:09+0000",
+      "to": "2021-08-22T20:25:09+0000"
+    },
+    {
+      "thumbnail": "https://fakeimg.pl/60x60",
+      "title": "abc announcement",
+      "desc": "abc description",
+      "from": "2021-08-22T20:25:09+0000",
+      "to": "2021-08-22T20:25:09+0000"
+    },
+    {
+      "thumbnail": "https://fakeimg.pl/60x60",
+      "title": "pqr announcement",
+      "desc": "pqr description",
+      "from": "2021-08-22T20:25:09+0000",
+      "to": "2021-08-22T20:25:09+0000"
+    }
+  ],
+  "tiles": [
+    {
+      "order": 1,
+      "title": "Member Login",
+      "desc": "Manage your account",
+      "icon": "face",
+      "url": "www.google.com",
+      "backgroundColor": "#efcced"
+    },
+    {
+      "order": 2,
+      "title": "Schedule",
+      "desc": "calendar updates",
+      "icon": "schedule",
+      "url": "www.google.com",
+      "backgroundColor": "#efcced"
+    },
+    {
+      "order": 3,
+      "title": "Facebook",
+      "desc": "Follow us on Facebook",
+      "icon": "schedule",
+      "url": "www.google.com",
+      "backgroundColor": "#efcced"
+    },
+    {
+      "order": 4,
+      "title": "Card",
+      "desc": "Your membership",
+      "icon": "qr_code_scanner",
+      "url": "www.google.com",
+      "backgroundColor": "#efcced"
+    }
+  ],
+  "config": {
+    "primaryColor": "#796AC6",
+    "secondaryColor": "#FEB144",
+    "primaryTextColor": "#ffffff",
+    "secondaryTextColor": "#000000"
+  }
+};
