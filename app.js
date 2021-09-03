ea6d8d536f579d75c51389f48e9391b3a6a1ca40
omerquadrimohd <mohd.omerquadri@dsmartsys.in>
2021-09-03 15:33:20 +0530

    Update payload

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.get('/home', (req, res) => {
   const homePayload = require('./payloads/home');
   res.send(homePayload);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
